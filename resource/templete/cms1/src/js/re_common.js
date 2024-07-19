e97ae35dc23d7f3e8bc5675c3fb6034a89db40ed
최은 <neko83@technoblood.com>
2024-07-19 14:57:30 +0900

    비주얼 작업중

--- resource/templete/cms1/src/js/re_common.js
+++ resource/templete/cms1/src/js/re_common.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     
     // web
-    $('.re_gnb_wrap > .wrap_inner > .re_gnb').on('mouseover click', function() {
+    $('.re_gnb_wrap > .wrap_inner > .re_gnb > li').on('mouseover click', function() {
         $('.re_gnb_wrap').addClass('on');
 
         // return false;
     });
-    $('.re_gnb_wrap > .wrap_inner > .re_gnb').mouseleave(function(){
+    $('.re_gnb_wrap > .wrap_inner > .re_gnb > li').mouseleave(function(){
         $('.re_gnb_wrap').removeClass('on');
     });
 
@@ -14,17 +14,19 @@ $(document).ready(function () {
     const menuItems = document.querySelectorAll('.re_gnb_wrap > .wrap_inner > .re_gnb > li');
 
     menuItems.forEach(item => {
-    item.addEventListener('mouseenter', function() {
-        this.classList.add('on');
+        item.addEventListener('mouseenter', function() {
+            this.classList.add('on');
+        });
+
+        item.addEventListener('mouseleave', function() {
+            this.classList.remove('on');
+        });
+
+        item.addEventListener('click', function() {
+            this.classList.add('on');
+        });
     });
 
-    item.addEventListener('mouseleave', function() {
-        this.classList.remove('on');
-    });
-
-    item.addEventListener('click', function() {
-        this.classList.add('on');
-    });
-    });
+    
 
 });

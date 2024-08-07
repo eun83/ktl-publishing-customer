e50fe4edf36c9a6635f6cb792064659c8ffdfdc8
최은 <neko83@technoblood.com>
2024-08-07 16:31:05 +0900

    푸터수정중

--- html/resource/templete/cms1/src/js/re_main.js
+++ html/resource/templete/cms1/src/js/re_main.js
@@ -91,10 +91,11 @@ $(document).ready(function () {
     $('#slider-partners').slick({
         slide: 'div',		            // 슬라이드 되어야 할 태그 ex) div, li 
         infinite: true, 	          // 무한 반복 옵션	 
-        slidesToShow: 5,		        // 한 화면에 보여질 컨텐츠 개수
+        slidesToShow: 3,		        // 한 화면에 보여질 컨텐츠 개수
         slidesToScroll: 1,		      // 스크롤 한번에 움직일 컨텐츠 개수
         centerMode: true,
         centerPadding: '0px',
+        variableWidth: true,
         speed: 500,	              // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
         arrows: true, 		        // 옆으로 이동하는 화살표 표시 여부
         dots: false, 		            // 스크롤바 아래 점으로 페이지네이션 여부

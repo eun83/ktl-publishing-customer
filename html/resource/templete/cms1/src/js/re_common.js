4979cb9d6fd6f9cda3b9e6d506698e4a314ec96d
손원철 <plzhans@colorverse.co>
2024-08-09 16:08:10 +0900

    자바스크립트 기능 수정
    - 검색 버튼 form 태그 영역으로 이동 못하게 disabled 처리
    - gnb 메뉴 기능 작업
    - 검색 입력 박스 리셋 기능

--- html/resource/templete/cms1/src/js/re_common.js
+++ html/resource/templete/cms1/src/js/re_common.js
@@ -35,8 +35,18 @@ $(document).ready(function () {
     $('.btn_top_search_open').on('click', function() {
         $('.total_search').addClass('on');
     });
+    
+    $('.delete_term').on('click', function(e){
+        e.preventDefault();
 
-    $('#top-close-btn').on('click' ,function(e) { //클릭으로 하니 자꾸 에러페이지 뜸
+        const $me = $(this);
+        const emptyTarget = $me.attr('empty_target');
+        if(emptyTarget && emptyTarget != ''){
+            $(emptyTarget).val('').focus(); 
+        }
+    });
+
+    $('#top-close-btn').on('click', function(e) {
         e.preventDefault();
         $('.total_search').removeClass('on');
     });
@@ -49,5 +59,37 @@ $(document).ready(function () {
         $('.re_header > .re_allmenu').removeClass('on');
     })
 
+    $('.re_allmenu .re_gnb').find('ul > li > a').filter(function() {
+        return $(this).siblings('ul').length > 0;
+    }).each(function(i){
+        const $me = $(this);
+        //console.debug('['+(i)+']'+$me.text());
+        $me.closest('li').addClass('expand');
+    });
+
+    // 메뉴 전환 및 자식 메뉴
+    $('.re_allmenu .re_gnb li a').on('click', function(e){
+        const $me = $(this);
+        const href = $me.attr('href');
+        if(href != '' && href != '#'){
+            return false;
+        }
+
+        // siblings 대상이 있는 경우 부모요소에 on을 넣거나 후속처리
+        // - siblings: 형제 요소중에 대상이 있나 확인
+        let $target = $me.siblings('.re_gnb_depth01, .re_gnb_depth02, .re_gnb_depth03');
+        if($target.length > 0){
+            const $parent = $me.parent();
+            if($parent.hasClass('on')){
+                $parent.removeClass('on');
+            } else {
+                $me.closest('ul').find('li.on').removeClass('on'); // 기존 메뉴 닫기
+                $parent.addClass('on');
+            }
+        } else {
+            console.warn('href is empty. target.text=' + $me.text());
+        }
+        return false;
+    });
 
 });

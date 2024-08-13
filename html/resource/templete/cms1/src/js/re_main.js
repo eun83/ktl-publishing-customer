f80b80f067519ea6d7fdae19368c39d238cbb4b0
최은 <neko83@technoblood.com>
2024-08-13 15:09:12 +0900

    여백정리

--- html/resource/templete/cms1/src/js/re_main.js
+++ html/resource/templete/cms1/src/js/re_main.js
@@ -1,23 +1,5 @@
 $(document).ready(function () {
     
-    
-//css 테스트 하느라 넣어둔거
-    // const menuItems = document.querySelectorAll('.visual_slider > .visual_content');
-
-    // menuItems.forEach(item => {
-    //     item.addEventListener('mouseenter', function() {
-    //         this.classList.add('on');
-    //     });
-
-    //     item.addEventListener('mouseleave', function() {
-    //         this.classList.remove('on');
-    //     });
-
-    //     item.addEventListener('click', function() {
-    //         this.classList.add('on');
-    //     });
-    // });
-
     // 비주얼 슬라이더
     $('#slider_visual').on('init', function(_event, slick){
         slick.options.appendArrows.find('.stop').click(function(){
